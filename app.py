68258d6bceaa6164e5e721b07ac6356b6787f9b9
Ammar-Alyasr <ammar.ahmet@gmail.com>
2017-10-20 01:27:13 -0700

    edits

--- app.py
+++ app.py
@@ -43,16 +43,6 @@ def webhook():
                         send_message(sender_id, "merhaba dostum :)")
                         send_message(sender_id, "kisa bir anket doldurmak ister misin ? ")
                         send_message(sender_id, "bir iki dakkika surer.. merak etme, tamam yaz baslayalim :)")
-                        if messaging_event.get("message"):
-                            if(message_text == "tamam"):
-                                send_message(sender_id, "harika..")
-                                send_message(sender_id, "Internet temelli dersler çok faydalıdır")
-                                send_message(sender_id, "1; Kesinlikle katılmıyorum, 5: kesinlikle katılıyorum olacak şekilde sana uygun şekilde yanıtla. ")
-                                if messaging_event.get("message"):
-                                    send_message(sender_id, "Proje temelli öğretim uygulamaları öğrenmeyi etkinleştirir")
-                            else:
-                                send_message(sender_id, "sen bilirsin :(")
-                                send_message(sender_id, "ben gidiyorum, gorusuruz")
 
                     else:
                         send_message(sender_id, message_text)
