cdb54cd23e59de83656cc40f22c3a1f25640d6dd
Ammar-Alyasr <ammar.ahmet@gmail.com>
2018-02-20 19:48:53 +0300

    sending image template

--- app.py
+++ app.py
@@ -33,25 +33,27 @@ def webhook():
 
                 try:
                     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"][
-                        "id"]  # the recipient's ID, which should be your page's facebook ID
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
 
                     if messaging_event.get('message'):  # someone sent us a message
                         if 'text' in messaging_event['message']:
                             message_text = messaging_event["message"]["text"]  # the message's text
                             send_message(sender_id,  message_text)
                             send_image_tamplate(sender_id)
+
                         elif 'attachments' in messaging_event['message']:
                             send_image(sender_id, "http://thecatapi.com/api/images/get?format=src&type=gif")
                             if sender_id == "1668676606538319":
                                 send_message('1668676606538319', "Hello ammarik")
+
                     if messaging_event.get("postback"):
                         if messaging_event['postback']['payload'] == "bunu":
                             send_message(sender_id,"Bunu mu istediniz Simdi gel")
 
                         if messaging_event['postback']['payload'] == "dersinix":
                             send_message(sender_id,"Buna ne dersiniz Simdi gel")
-
+                        if messaging_event['postback']['payload'] == "template":
+                            send_message(sender_id, "Menu eklendi")
                     if messaging_event.get("delivery"):  # delivery confirmation
                         pass
 
@@ -177,7 +179,7 @@ def send_image_tamplate(recipient_id):
                     "elements": [
                         {
                             "title": "Welcome to Peter'\''s Hats",
-                            "image_url": "http://thecatapi.com/api/images/get?format=src&type=gif",
+                            "image_url": "http://thecatapi.com/api/images/get?format=src&type=jpg",
                             "subtitle": "We'\''ve got the right hat for everyone.",
                             "default_action": {
                                 "type": "web_url",
@@ -190,7 +192,7 @@ def send_image_tamplate(recipient_id):
                                 {
                                     "type": "postback",
                                     "title": "Start Chatting",
-                                    "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                                    "payload": "template"
                                 },
                             ]
                         }
