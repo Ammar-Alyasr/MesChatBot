42551f5b3ca8e28d3fcf60913f5e62ca334bab67
Ammar-Alyasr <ammar.ahmet@gmail.com>
2018-02-27 11:08:31 +0300

    Rich msg

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def webhook():
                         if 'text' in messaging_event['message']:
                             message_text = messaging_event["message"]["text"]  # the message's text
                             send_message(sender_id,  message_text)
+                            send_multi_template(sender_id)
 
                         elif 'attachments' in messaging_event['message']:
                             send_image(sender_id, "http://thecatapi.com/api/images/get?format=src&type=gif")
@@ -46,11 +47,11 @@ def webhook():
                                 send_message('1668676606538319', "Hello ammarik")
 
                     if messaging_event.get("postback"):
-                        if messaging_event['postback']['payload'] == "bunu":
-                            send_message(sender_id,"Bunu mu istediniz Simdi gel")
+                        if messaging_event['postback']['payload'] == "bir_secildi":
+                            send_message(sender_id,"Biri sectiniz")
 
-                        if messaging_event['postback']['payload'] == "dersinix":
-                            send_message(sender_id,"Buna ne dersiniz Simdi gel")
+                        if messaging_event['postback']['payload'] == "iki_secildi":
+                            send_message(sender_id,"ikiyi sectiniz")
                         if messaging_event['postback']['payload'] == "template":
                             send_message(sender_id, "Menu eklendi")
                     if messaging_event.get("delivery"):  # delivery confirmation
@@ -153,6 +154,86 @@ def send_general_template(recipient_id):
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
+
+def send_multi_template(recipient_id):
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Ai Chat Bot Communities",
+                    "subtitle": "Communities to Follow",
+                    "image_url": "http://1u88jj3r4db2x4txp44yqfj1.wpengine.netdna-cdn.com/wp-content/uploads/2016/04/chatbot-930x659.jpg",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.facebook.com/groups/aichatbots/",
+                        "title": "FB Chatbot Group"
+                    }, {
+                        "type": "web_url",
+                        "url": "https://www.reddit.com/r/Chat_Bots/",
+                        "title": "Chatbots on Reddit"
+                    },{
+                        "type": "web_url",
+                        "url": "https://twitter.com/aichatbots",
+                        "title": "Chatbots on Twitter"
+                    }],
+                }, {
+                    "title": "Chatbots FAQ",
+                    "subtitle": "Aking the Deep Questions",
+                    "image_url": "https://tctechcrunch2011.files.wordpress.com/2016/04/facebook-chatbots.png?w=738",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "What's the benefit?",
+                        "payload": "Chatbots make content interactive instead of static",
+                    },{
+                        "type": "postback",
+                        "title": "What can Chatbots do",
+                        "payload": "One day Chatbots will control the Internet of Things! You will be able to control your homes temperature with a text",
+                    }, {
+                        "type": "postback",
+                        "title": "The Future",
+                        "payload": "Chatbots are fun! One day your BFF might be a Chatbot",
+                    }],
+                },  {
+                    "title": "Learning More",
+                    "subtitle": "Aking the Deep Questions",
+                    "image_url": "http://www.brandknewmag.com/wp-content/uploads/2015/12/cortana.jpg",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "AIML",
+                        "payload": "Checkout Artificial Intelligence Mark Up Language. Its easier than you think!",
+                    },{
+                        "type": "postback",
+                        "title": "Machine Learning",
+                        "payload": "Use python to teach your maching in 16D space in 15min",
+                    }, {
+                        "type": "postback",
+                        "title": "Communities",
+                        "payload": "Online communities & Meetups are the best way to stay ahead of the curve!",
+                    }],
+                }]
+            }
+        }
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
 
 
 def log(message):  # simple wrapper for logging to stdout on heroku
