9a0d06096550efc3e1513672645a1943231eee64
Ammar-Alyasr <ammar.ahmet@gmail.com>
2018-05-13 21:17:12 +0300

    dogruluk orani

--- Sources/data/wit_response.py
+++ Sources/data/wit_response.py
@@ -3,7 +3,7 @@ def wit_response(data):
     nlp = data # messaging_event["message"]["nlp"]["entities"]
     dic = {}
     for r in nlp:
-        if nlp[r][0]["confidence"] > 0.6: #dogrulama orani
+        if nlp[r][0]["confidence"] > 0.5: #dogrulama orani
             dic[nlp[r][0]["value"]] = nlp[r][0]["confidence"]
 
     if bool(dic) != False:
