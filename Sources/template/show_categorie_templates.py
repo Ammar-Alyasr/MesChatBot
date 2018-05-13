d269d55492abfb63ae17fb65b86e9bef2d6e12bb
Ammar-Alyasr <ammar.ahmet@gmail.com>
2018-05-13 18:34:03 +0300

    wit.ai response

--- Sources/template/show_categorie_templates.py
+++ Sources/template/show_categorie_templates.py
@@ -11,7 +11,7 @@ fincan_cay = "http://www.kazanabil.com/pasabahce-vela-12-parca-fincan-takimi-525
 bitki_cay = "https://www.pirelikedi.com/wp-content/uploads/2017/11/karisik-bitki-cayi-adet-sancisi.jpg"
 su = "http://www.bik.gov.tr/wp-content/uploads/2017/05/su.jpg"
 soda = "https://cdnph.upi.com/svc/sv/upi_com/8041492971723/2017/1/ed977c35005a74ee81bf9b0dbd8b0c8a/Study-links-diet-soda-to-stroke-and-dementia-risks.jpg"
-
+nescafe = "http://cdn.shopify.com/s/files/1/1162/6052/products/product-image-mug_3_1024x1024.png?v=1520464037"
 def categories_template(recipient_id):
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
@@ -51,7 +51,7 @@ def categories_template(recipient_id):
                         "buttons": [{
                             "type": "postback",
                             "title": "Ürünleri Getir",
-                            "payload": "cofee_categorie",
+                            "payload": "coffee_categorie",
                         },
                         ],
                     },
@@ -169,6 +169,96 @@ def show_teas(recipient_id):
                             },
                             ],
                         },
+
+                    ]
+                }
+            }
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+
+
+
+def show_coffee(recipient_id):
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "list",
+                    # compact or large
+                    "top_element_style": "compact",
+                    # start menu of template
+                    "elements": [{
+                        # first
+                        "title": "Sade Kahve",
+                        "image_url": kahve,
+                        # buttons of menu
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Sepeteye Ekle",
+                            "payload": "add_sade_coffee"
+                        }, ],
+                    },
+                        {
+                        # second
+                        "title": "Orta Kahve",
+                        "image_url": kahve,
+
+                        # buttons of menu
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Sepeteye Ekle",
+                            "payload": "add_orta_kahve",
+                        },
+                        ],
+                    },
+                        {
+                        # 3d menu
+                        "title": "Şekerli Kahve",
+                        "image_url": kahve,
+
+                        # buttonus of menu
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Sepeteye Ekle",
+                            "payload": "add_sekerli_kahve",
+                        },
+                        ],
+                    },
+                        {
+                            # 4th menu
+                            "title": "Nescafe Sade",
+                            "image_url": nescafe,
+                            # buttonus of menu
+                            "buttons": [{
+                                "type": "postback",
+                                "title": "Sepeteye Ekle",
+                                "payload": "add_sade_nescafe",
+                            },
+                            ],
+                        },
+                        {
+                            # 4th menu
+                            "title": "Nescafe Sütlü",
+                            "image_url": nescafe,
+                            # buttonus of menu
+                            "buttons": [{
+                                "type": "postback",
+                                "title": "Sepeteye Ekle",
+                                "payload": "add_sutlu_nescafe",
+                            },
+                            ],
+                        }
 
                     ]
                 }
