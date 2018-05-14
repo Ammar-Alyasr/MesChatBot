ed441a1784797a27bbefe4d8e6fa8cd1a8cc7a90
Ammar-Alyasr <ammar.ahmet@gmail.com>
2018-05-14 21:45:09 +0300

    receipt

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ def verify():
     return "Hello world", 200
 
 
+
+
 @app.route('/', methods=['POST'])
 def webhook():
     # endpoint for processing incoming messaging events
@@ -59,6 +61,8 @@ def webhook():
 
                             message_text = messaging_event["message"]["text"]  # the message's text
 
+                            if message_text == "ammarik":
+                                send_receipt(sender_id)
                             if message_text in numbers:
                                 basket_process.check_last_order(sender_id, message_text)
 
@@ -209,6 +213,77 @@ def webhook():
                     send_message(sender_id, "sen ne attin ya!!!!" + str(e))
 
     return "ok", 200
+
+
+def send_receipt(sender_id):
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient":{
+        "id": sender_id
+      },
+      "message":{
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"receipt",
+            "recipient_name":"Stephane Crozatier",
+            "order_number":"12345678902",
+            "currency":"USD",
+            "payment_method":"Visa 2345",
+            "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
+            "timestamp":"1428444852",
+            "address":{
+              "street_1":"1 Hacker Way",
+              "street_2":"",
+              "city":"Menlo Park",
+              "postal_code":"94025",
+              "state":"CA",
+              "country":"US"
+            },
+            "summary":{
+              "subtotal":75.00,
+              "shipping_cost":4.95,
+              "total_tax":6.19,
+              "total_cost":56.14
+            },
+            "adjustments":[
+              {
+                "name":"New Customer Discount",
+                "amount":20
+              },
+              {
+                "name":"$10 Off Coupon",
+                "amount":10
+              }
+            ],
+            "elements":[
+              {
+                "title":"Classic White T-Shirt",
+                "subtitle":"100% Soft and Luxurious Cotton",
+                "quantity":2,
+                "price":50,
+                "currency":"USD",
+                "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png"
+              },
+              {
+                "title":"Classic Gray T-Shirt",
+                "subtitle":"100% Soft and Luxurious Cotton",
+                "quantity":1,
+                "price":25,
+                "currency":"USD",
+                "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
+              }
+            ]
+          }
+        }
+      }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
 
 
 def send_message(recipient_id, message_text):
