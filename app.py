f835dace1b56a3bd8cdcccd71cf5cca571168387
Ammar-Alyasr <ammar.ahmet@gmail.com>
2018-05-16 16:55:20 +0300

    finish_shopping

--- app.py
+++ app.py
@@ -68,10 +68,7 @@ def webhook():
                                 if message_text == "dinaram":
                                     send_receipt(sender_id)
                                 if message_text in numbers:
-                                    if basket_process.check_last_order(sender_id, message_text) == "":
-                                        basket_process.check_last_order(sender_id, message_text)
-                                    else:
-                                        send_message(sender_id, (message_text , "+ 5 = ", int(message_text)+5))
+                                    basket_process.check_last_order(sender_id, message_text)
 
                                 send_message(sender_id, "Yeniden Hosgeldiniz")
                                 categorie_quick_replie(sender_id)
